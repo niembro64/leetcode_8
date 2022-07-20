0982f77820205e6c677823299581fc308207f170
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-07-20 00:12:50 -0400

    not the best but

--- leetcode_8.js
+++ leetcode_8.js
@@ -2,22 +2,87 @@ function myAtoi(s) {
     if (s.length === 0) {
         return 0;
     }
-    var pos = true;
-    var numberStarted = false;
-    var newString = "";
+    var p = true;
+    var index = -1;
+    var n = "";
     for (var i = 0; i < s.length; i++) {
-        if (s[i] === "+" ||
-            s[i] === "-" ||
-            (s[i].charCodeAt(0) >= 48 && s[i].charCodeAt(0) <= 57)) {
-            newString += s.slice(i, 1);
-        }
-        else {
+        if (s[i] !== " ") {
+            index = i;
+            break;
         }
     }
-    return parseInt(newString);
+    if (index === -1) {
+        return 0;
+    }
+    if (s[index] === "-") {
+        p = false;
+    }
+    else if (s[index] === "+") {
+    }
+    else if (s[index].charCodeAt(0) >= 48 && s[index].charCodeAt(0) <= 57) {
+        n += s[index];
+    }
+    else {
+        return 0;
+    }
+    index++;
+    if (s[index] &&
+        s[index].charCodeAt(0) >= 48 &&
+        s[index].charCodeAt(0) <= 57) {
+        n += s[index];
+    }
+    else if (s[index - 1] &&
+        s[index - 1].charCodeAt(0) >= 48 &&
+        s[index - 1].charCodeAt(0) <= 57) {
+        return p ? parseInt(n) : parseInt(n) * -1;
+    }
+    else {
+        return 0;
+    }
+    index++;
+    while (s[index]) {
+        if (s[index].charCodeAt(0) >= 48 && s[index].charCodeAt(0) <= 57) {
+            n += s[index];
+        }
+        else {
+            break;
+        }
+        index++;
+    }
+    if (p) {
+        if (parseInt(n) > Math.pow(2, 31) - 1) {
+            return Math.pow(2, 31) - 1;
+        }
+    }
+    else {
+        if (parseInt(n) > Math.pow(2, 31)) {
+            return Math.pow(2, 31) * -1;
+        }
+    }
+    return p ? parseInt(n) : parseInt(n) * -1;
 }
-var x = ["0", "-1", " 980 ", " -d8 ", "  -10XE", "913"];
+var x = [
+    " ",
+    "0",
+    "  d",
+    " d",
+    "d",
+    "-1",
+    " +980 ",
+    " -d8 ",
+    "  -10XE",
+    "913",
+    "  -",
+    "  -42",
+    "42",
+    (Math.pow(2, 31) + 10).toString(),
+    ((Math.pow(2, 31) + 10) * -1).toString(),
+    "1",
+];
 for (var i = 0; i < x.length; i++) {
-    console.log(x[i], "|", myAtoi(x[i]));
+    console.log("STRING:", x[i]);
+    console.log(myAtoi(x[i]));
     console.log("_________");
 }
+console.log(0, "0".charCodeAt(0));
+console.log(9, "9".charCodeAt(0));
--- leetcode_8.ts
+++ leetcode_8.ts
@@ -2,27 +2,92 @@ function myAtoi(s: string): number {
     if (s.length === 0) {
         return 0;
     }
-    let pos: boolean = true;
-    let numberStarted: boolean = false;
-    let newString: string = "";
+    let p: boolean = true;
+    let index: number = -1;
+    let n: string = "";
 
     for (let i = 0; i < s.length; i++) {
-        if (
-            s[i] === "+" ||
-            s[i] === "-" ||
-            (s[i].charCodeAt(0) >= 48 && s[i].charCodeAt(0) <= 57)
-        ) {
-            newString += s.slice(i, 1);
-        } else {
+        if (s[i] !== " ") {
+            index = i;
+            break;
         }
     }
 
-    return parseInt(newString);
+    if (index === -1) {
+        return 0;
+    }
+
+    if (s[index] === "-") {
+        p = false;
+    } else if (s[index] === "+") {
+    } else if (s[index].charCodeAt(0) >= 48 && s[index].charCodeAt(0) <= 57) {
+        n += s[index];
+    } else {
+        return 0;
+    }
+
+    index++;
+
+    if (
+        s[index] &&
+        s[index].charCodeAt(0) >= 48 &&
+        s[index].charCodeAt(0) <= 57
+    ) {
+        n += s[index];
+    } else if (
+        s[index - 1] &&
+        s[index - 1].charCodeAt(0) >= 48 &&
+        s[index - 1].charCodeAt(0) <= 57
+    ) {
+        return p ? parseInt(n) : parseInt(n) * -1;
+    } else {
+        return 0;
+    }
+    index++;
+    while (s[index]) {
+        if (s[index].charCodeAt(0) >= 48 && s[index].charCodeAt(0) <= 57) {
+            n += s[index];
+        } else {
+            break;
+        }
+        index++;
+    }
+    if (p) {
+        if (parseInt(n) > Math.pow(2, 31) - 1) {
+            return Math.pow(2, 31) - 1;
+        }
+    } else {
+        if (parseInt(n) > Math.pow(2, 31)) {
+            return Math.pow(2, 31) * -1;
+        }
+    }
+    return p ? parseInt(n) : parseInt(n) * -1;
 }
 
-const x: string[] = ["0", "-1", " 980 ", " -d8 ", "  -10XE", "913"];
+const x: string[] = [
+    " ",
+    "0",
+    "  d",
+    " d",
+    "d",
+    "-1",
+    " +980 ",
+    " -d8 ",
+    "  -10XE",
+    "913",
+    "  -",
+    "  -42",
+    "42",
+    (Math.pow(2, 31) + 10).toString(),
+    ((Math.pow(2, 31) + 10) * -1).toString(),
+    "1",
+];
 
 for (let i = 0; i < x.length; i++) {
-    console.log(x[i], "|", myAtoi(x[i]));
+    console.log("STRING:", x[i]);
+    console.log(myAtoi(x[i]));
     console.log("_________");
 }
+
+console.log(0, "0".charCodeAt(0));
+console.log(9, "9".charCodeAt(0));
